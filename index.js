23f58b2887691f02965af19a0a00900592dc2916
Oleksandr Peshkov <oleksandr.peshkov@gtl.net>
2025-04-23 14:05:40 +0300

    add api call

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ document.addEventListener("visibilitychange", () => {
   
   if (document.hidden) {
       p.innerHTML = "Document is hidden"
+      fetch("https://api.api-ninjas.com/v1/counter?id=become_hidden&hit=true", {
+        method: "GET",
+        headers: {
+            "X-Api-Key": "n58t7xzI5s0/z6oOmzTRFw==UWqdyXHd03ufoYCJ"
+        }
+      }).then(() => console.log(success));
       logsContainer.appendChild(p)
   } else {
       p.innerHTML = "Document is visible"
